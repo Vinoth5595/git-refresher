3eb5f2b41f582a517fc05c4cbc7af21c136e093e
Vinoth Manoharan <vinothm.manoharan@gmail.com>
2021-03-20 00:37:40 +0530

    version 5

--- src/sample/Sample.java
+++ src/sample/Sample.java
@@ -3,7 +3,7 @@ package sample;
 public class Sample {
 
 	public static void main(String[] args) {
-		String version = "4";
+		String version = "5";
 		System.out.println("I am in Master Branch");
 	}
 
