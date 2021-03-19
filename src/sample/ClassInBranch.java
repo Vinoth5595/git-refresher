8ddbfe7858bb5b9953faff0b8f3c2cb9a9fd43b6
Vinoth Manoharan <vinothm.manoharan@gmail.com>
2021-03-19 23:58:11 +0530

    change

--- src/sample/ClassInBranch.java
+++ src/sample/ClassInBranch.java
@@ -3,7 +3,7 @@ package sample;
 public class ClassInBranch {
 
 	public void nothing() {
-		System.out.println("bug fix");
+		System.out.println("sad");
 		System.out.println("Added from git");
 	}
 }
