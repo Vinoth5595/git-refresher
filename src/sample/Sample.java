f954ccd865cdb773b2d78a75d293dc7c741a5754
vinoth <vinoth@vinoth>
2021-03-20 00:23:54 +0530

    version 4

--- src/sample/Sample.java
+++ src/sample/Sample.java
@@ -3,7 +3,7 @@ package sample;
 public class Sample {
 
 	public static void main(String[] args) {
-		String version = "2";
+		String version = "4";
 		System.out.println("I am in Master Branch");
 	}
 
