43c3798029751f2694e277b22b679830bb5f7c70
Vinoth Manoharan <vinothm.manoharan@gmail.com>
2021-10-04 16:30:02 +0530

    changed value

--- src/sample/Sample.java
+++ src/sample/Sample.java
@@ -3,7 +3,7 @@ package sample;
 public class Sample {
 
 	public static void main(String[] args) {
-		String version = "6";
+		String version = "76";
 		System.out.println("I am in Master Branch");
 	}
 
